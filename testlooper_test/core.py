fa9a8c28e02b2235892ba75c57fe7399beb9286b
William Grant <william.grant@aprioriinvestments.com>
2023-09-14 18:16:19 -0400

    add one more test

--- testlooper_test/core.py
+++ testlooper_test/core.py
@@ -1,6 +1,9 @@
 def main() -> str:
     return "Hello World!"
 
+def alternate() -> str:
+    return 'Greetings'
+
 if __name__ == "__main__":
     main()
 
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 import pytest
 import testlooper_test.core
 
+
 @pytest.mark.group_one
 def test_core_function_one():
     result = testlooper_test.core.main()
@@ -11,4 +12,12 @@ def test_core_function_one():
 def test_core_function_two():
     with pytest.raises(AssertionError):
         result = testlooper_test.core.main()
-        assert result == "Goodbye World!", "The result did not match the expected output"
+        assert (
+            result == "Goodbye World!"
+        ), "The result did not match the expected output"
+
+
+@pytest.mark.group_one
+def test_core_function_three():
+    result = testlooper_test.core.alternate()
+    assert result == "Greetings", "The result did not match the expected output"
